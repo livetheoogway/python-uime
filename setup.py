c4057bb7f1b3ac5f463192aed4b2fdb8907a7aa9
Tushar Naik <tushar.knaik@gmail.com>
2023-12-09 16:25:39 +0530

    Auto incrementing version to 1.0.2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='python-uime',
-    version='1.0.0',
+    version='1.0.2',
     packages=find_packages(),
     license=' License 2.0',
     description='A python library to help you build simple UI forms for your python functions',
